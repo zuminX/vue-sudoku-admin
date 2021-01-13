2ccdcf71a5bee0e020013819f7c8e1153c8c052c
zuminX <1215287120@qq.com>
2021-01-13 20:56:51 +0800

    移除分析插件

--- vue.config.js
+++ vue.config.js
@@ -59,18 +59,6 @@ module.exports = {
         threshold: 10240,
         minRatio: 0.8
       }),
-      new BundleAnalyzerPlugin({
-        analyzerMode: 'server',
-        analyzerHost: 'localhost',
-        analyzerPort: 10000,
-        reportFilename: 'report.html',
-        defaultSizes: 'parsed',
-        openAnalyzer: false,
-        generateStatsFile: false,
-        statsFilename: 'stats.json',
-        statsOptions: null,
-        logLevel: 'info'
-      })
     ]
     // 打包优化，去除console.log
     config.optimization.minimizer.push(new UglifyJsPlugin({
